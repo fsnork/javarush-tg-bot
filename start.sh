397794a5cc39a411dce128b8de5ad451ec623511
fsnork <m@ej3.ru>
2021-05-17 16:07:12 +0300

    JRTB-13: implement deployment process via docker

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ mvn package
 docker-compose stop
 
 # Add environment variables
-export BOT_NAME=jr_testBot
-export BOT_TOKEN=1831832718:AAFI7tlkKasaE-_BcbEhDvICSAzrke2c6uw
+# export BOT_NAME=jr_testBot
+# export BOT_TOKEN=1831832718:AAFI7tlkKasaE-_BcbEhDvICSAzrke2c6uw
 
 # Start new deployment
 docker-compose up --build -d
